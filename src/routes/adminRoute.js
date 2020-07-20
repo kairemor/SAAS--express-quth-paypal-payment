4c93f591205f499f68eebee8fa086bfb676c6af1
kmtc44 <kairmor@yahoo.com>
2020-07-20 14:01:55 +0000

    admin dashboard evolution

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import {ensureAuthenticated} from '../lib/authenticate';
-import * as authentication from "../lib/authenticate";
+import Model from '../models';
+import { findAll } from "../services";
 
-
+const { User, Group } = Model; 
 const router = Router();
 
 // Dashboard
@@ -10,6 +11,28 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('index', {
     user: req.user
   })
+);
+// Users
+router.get('/users', ensureAuthenticated, async(req, res) => {
+  const users = await findAll(User)
+  res.render('users', {
+    users: users
+  })
+  }
+);
+
+router.get('/create-user', ensureAuthenticated, async(req, res) => {
+  res.render('addUser', {})
+  }
+);
+
+// group
+router.get('/group', ensureAuthenticated, async(req, res) => {
+  const groups = await findAll(Group)
+  res.render('users', {
+    groups: groups
+  })
+  }
 );
 
 export default router;
