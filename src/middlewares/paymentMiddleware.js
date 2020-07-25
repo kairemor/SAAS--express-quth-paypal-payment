5422a357a33e71746d9f877ea83988d19cf30b65
kmtc44 <kairmor@yahoo.com>
2020-07-25 11:21:06 +0000

    middleware check profileid

--- src/middlewares/paymentMiddleware.js
+++ src/middlewares/paymentMiddleware.js
@@ -16,30 +16,32 @@ const {
 
 export const checkSubscription = async (req, res, next) => {
   const token = await getToken()
-  axios.get(`${baseAPIUrl}/billing/subscriptions`)
-    .then(async (subscription) => {
-      if (subscription.data.status === 'APPROVAL' && !req.user.isSubscribed) {
-        await update(User, req.user.id, {
-          isSubscribed: true
-        })
-        fs.readFile('subscription_info.json', (error, data) => {
-          if (error) {
-            res.status(400).json({
-              status: 'error',
-              message: error
-            })
-          }
+  if (req.user.profileId) {
+    axios.get(`${baseAPIUrl}/billing/subscriptions/${req.user.profileId}`)
+      .then(async (subscription) => {
+        if (subscription.data.status === 'APPROVAL' && !req.user.isSubscribed) {
+          await update(User, req.user.id, {
+            isSubscribed: true
+          })
+          fs.readFile('subscription_info.json', (error, data) => {
+            if (error) {
+              res.status(400).json({
+                status: 'error',
+                message: error
+              })
+            }
 
-          const plans = JSON.parse(data)
+            const plans = JSON.parse(data)
 
-        })
-      }
-    }, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-    .catch(err => console.log(err))
+          })
+        }
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .catch(err => console.log(err))
+  }
 
   next()
 }
