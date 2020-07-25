1f1f7d251f6dd4f28d6a0f3a725126b0e943924e
kmtc44 <kairmor@yahoo.com>
2020-07-25 19:22:13 +0000

    docker add

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12.18.2-alpine3.9
+EXPOSE 3000 9229
+
+WORKDIR /home/app
+
+COPY package.json /home/app/
+COPY package-lock.json /home/app/
+
+RUN npm install 
+
+COPY . /home/app
+COPY .env home/app
+
+CMD ["npm", "start"]
--- src/models/index.js
+++ src/models/index.js
@@ -7,14 +7,15 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = configs[env];
 const db = {};
-const sequelize = config.use_env_variable
-  ? new Sequelize(config.url, config)
-  : new Sequelize(config.database, config.username, config.password, config);
+console.log(config)
+const sequelize = config.use_env_variable ?
+  new Sequelize(config.url, config) :
+  new Sequelize(config.database, config.username, config.password, config);
 fs.readdirSync(__dirname)
   .filter(
     file =>
-      // eslint-disable-next-line implicit-arrow-linebreak
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    // eslint-disable-next-line implicit-arrow-linebreak
+    file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
